8085108f8b12a83311a01e68be12cc805237ef21
jian-kowalski <jian_kowalski@hotmail.com>
2021-08-25 19:31:21 -0300

    Tratamento de exceptions

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -11,19 +11,9 @@ class CreateUserService{
 
     async execute({name, email, admin}: IUserRequest){
         const usersRepository = getCustomRepository(UsersRepositories);
+
+        await this.validarEmail(email, usersRepository);   
         
-        if (!email){
-            throw new Error("Email incorrect");
-        }
-
-        const userAlreadyExists = await usersRepository.findOne({
-            email,    
-            
-        }); 
-        if (userAlreadyExists) {
-            throw new Error("User already existis");
-        }   
-
         const user = usersRepository.create({
             name,
             email,
@@ -34,6 +24,18 @@ class CreateUserService{
         return user;
     }
 
+
+    private async validarEmail(email: string, usersRepository: UsersRepositories) {
+        if (!email) {
+            throw new Error("Email incorrect");
+        }
+
+        const userAlreadyExists = await usersRepository.findOne({email,});
+
+        if (userAlreadyExists) {
+            throw new Error("User already existis");
+        }
+    }
 }
 
 export { CreateUserService };
