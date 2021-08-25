3331026e391d592f8099aeca3b290142096038bc
jian-kowalski <jian_kowalski@hotmail.com>
2021-08-25 20:53:38 -0300

    Criação do campo password e adicionando o jwt

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -5,10 +5,11 @@ interface IUserRequest {
   name: string;
   email: string;
   admin?: boolean;
+  password: string
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, password,  admin }: IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
 
     await this.validarEmail(email, usersRepository);
@@ -17,6 +18,7 @@ class CreateUserService {
       name,
       email,
       admin,
+      password
     });
 
     await usersRepository.save(user);
